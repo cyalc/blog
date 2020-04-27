b4a55c891a695dff264d34e1c7a07dc53d2e14bd
cyalc <cyalcinsoy@gmail.com>
2020-04-27 13:02:44 +0200

    Add header and other social links

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,31 @@ import SocialLink from "../components/SocialLink";
 
 const IndexPage = () => (
     <main>
+        <h1>cyalc.dev</h1>
         <SocialLink
             name="linkedin"
-            url="https://linkedin.com"
+            url="https://www.linkedin.com/in/cyalcinsoy/"
             iconPath="/linkedin.svg"
         />
-        <style jsx>{``}</style>
+        <SocialLink
+            name="twitter"
+            url="https://twitter.com/schalbo"
+            iconPath="/twitter.svg"
+        />
+        <SocialLink
+            name="github"
+            url="https://github.com/cyalc"
+            iconPath="/github.svg"
+        />
+        <style jsx>
+            {`
+                h1 {
+                    font-family: Courier New;
+                    font-size: 3em;
+                    color: #ffffff;
+                }
+            `}
+        </style>
         <style global jsx>{`
             body {
                 background: #272822;
