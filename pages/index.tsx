2d7d0762f58ec9aed7a5770da8ac99e1c89347cd
cyalc <cyalcinsoy@gmail.com>
2022-11-10 22:22:03 +0100

    update dependency

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import SocialLink from "../components/SocialLink";
 import Head from "next/head";
+import { Analytics } from '@vercel/analytics/react';
 
 type Props = {
     siteConfig: SiteConfig
@@ -11,38 +12,39 @@ type SiteConfig = {
 }
 
 const IndexPage = ({ siteConfig }: Props) => (
-    <div>
-        <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="description" content={siteConfig.pageDescription} />
-            <title>{siteConfig.pageTitle}</title>
-            <link
-                href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&display=swap"
-                rel="stylesheet"
-            />
-        </Head>
-        <div className="headlineContainer">
-            <div className="space"></div>
-            <h1>{siteConfig.pageTitle}</h1>
-            <div className="socialContainer">
-                <SocialLink
-                    name="linkedin"
-                    url="https://www.linkedin.com/in/cyalcinsoy/"
-                    iconPath="/linkedin.svg"
+    <>
+        <div>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content={siteConfig.pageDescription} />
+                <title>{siteConfig.pageTitle}</title>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&display=swap"
+                    rel="stylesheet"
                 />
-                <SocialLink
-                    name="twitter"
-                    url="https://twitter.com/schalbo"
-                    iconPath="/twitter.svg"
-                />
-                <SocialLink
-                    name="github"
-                    url="https://github.com/cyalc"
-                    iconPath="/github.svg"
-                />
-            </div>
-            <div className="space"></div>
-            <style jsx>{`
+            </Head>
+            <div className="headlineContainer">
+                <div className="space"></div>
+                <h1>{siteConfig.pageTitle}</h1>
+                <div className="socialContainer">
+                    <SocialLink
+                        name="linkedin"
+                        url="https://www.linkedin.com/in/cyalcinsoy/"
+                        iconPath="/linkedin.svg"
+                    />
+                    <SocialLink
+                        name="twitter"
+                        url="https://twitter.com/schalbo"
+                        iconPath="/twitter.svg"
+                    />
+                    <SocialLink
+                        name="github"
+                        url="https://github.com/cyalc"
+                        iconPath="/github.svg"
+                    />
+                </div>
+                <div className="space"></div>
+                <style jsx>{`
                 h1 {
                     font-family: "Roboto Mono", monospace;
                     font-size: 3em;
@@ -62,15 +64,17 @@ const IndexPage = ({ siteConfig }: Props) => (
                     flex: 1;
                 }
             `}</style>
-            <style jsx global>{`
+                <style jsx global>{`
                 html,
                 body {
                     background-color: #272822;
                     margin: 0;
                 }
             `}</style>
+            </div>
         </div>
-    </div>
+        <Analytics />
+    </>
 );
 
 export default IndexPage;
