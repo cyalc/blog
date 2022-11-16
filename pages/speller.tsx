0d1d9d1fe44d2143c88056af03ac02b146bb6f26
cyalc <cyalcinsoy@gmail.com>
2022-11-16 22:51:25 +0100

    Add speller prototype

--- pages/speller.tsx
+++ pages/speller.tsx
@@ -1,21 +1,55 @@
 import Head from "next/head";
+import { useState } from "react";
 import styled from "styled-components";
+import { codify } from "../apps/speller/models";
 
-const StyledComponent = styled.div`
-  background: black;
-  color: white;
+const VerticalLayout = styled("div")<{ alignItems: string }>`
+  display: flex;
+  flex-direction: column;
+  align-items: ${(props) => props.alignItems};
 `;
 
 function Speller() {
+  const [uiState, setUiState] = useState<UiState>({ codes: [] });
+
   return (
     <>
       <Head>
         <title>Spelling Alphabet</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <StyledComponent></StyledComponent>
+      <VerticalLayout alignItems="center">
+        <VerticalLayout alignItems="flex-start">
+          <input
+            type="text"
+            onChange={(e) =>
+              setUiState(buildState({ code: e.target.value.toLowerCase() }))
+            }
+          />
+
+          <ul>
+            {uiState.codes.map((code, index) => (
+              <li key={index}>{code}</li>
+            ))}
+          </ul>
+        </VerticalLayout>
+      </VerticalLayout>
     </>
   );
 }
 
+function buildState(action: Action): UiState {
+  return {
+    codes: codify(action.code),
+  };
+}
+
+type Action = {
+  code: string;
+};
+
 export default Speller;
+
+type UiState = {
+  codes: string[];
+};
