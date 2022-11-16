a9e3083034ff65c8d07223ff97859907e31f5a55
cyalc <cyalcinsoy@gmail.com>
2022-11-16 20:10:49 +0100

    Add prettier

--- components/SocialLink.tsx
+++ components/SocialLink.tsx
@@ -1,35 +1,35 @@
 type Props = {
-    name: string;
-    url: string;
-    iconPath: string;
+  name: string;
+  url: string;
+  iconPath: string;
 };
 
 const SocialLink: React.FunctionComponent<Props> = ({
-    name,
-    url,
-    iconPath,
+  name,
+  url,
+  iconPath,
 }) => (
-    <div className="socialLink">
-        <a href={url}>
-            <img src={iconPath} alt={name} />
-        </a>
-        <style jsx>{`
-            .socialLink {
-                display: inline-block;
-                padding: 16px;
-                background: none;
-                border: 0px;
-                cursor: pointer;
-                margin: 13px;
-            }
+  <div className="socialLink">
+    <a href={url}>
+      <img src={iconPath} alt={name} />
+    </a>
+    <style jsx>{`
+      .socialLink {
+        display: inline-block;
+        padding: 16px;
+        background: none;
+        border: 0px;
+        cursor: pointer;
+        margin: 13px;
+      }
 
-            .socialLink:hover {
-                background: black;
-                color: white;
-                border-radius: 8px;
-            }
-        `}</style>
-    </div>
+      .socialLink:hover {
+        background: black;
+        color: white;
+        border-radius: 8px;
+      }
+    `}</style>
+  </div>
 );
 
 export default SocialLink;
--- pages/speller.tsx
+++ pages/speller.tsx
@@ -1,21 +1,21 @@
 import Head from "next/head";
-import styled from "styled-components"
+import styled from "styled-components";
 
 const StyledComponent = styled.div`
-        background: black;
-        color: white;
-    `;
+  background: black;
+  color: white;
+`;
 
 function Speller() {
-    return (
-        <>
-            <Head>
-                <title>Spelling Alphabet</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            </Head>
-            <StyledComponent></StyledComponent>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Spelling Alphabet</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <StyledComponent></StyledComponent>
+    </>
+  );
 }
 
-export default Speller
+export default Speller;
