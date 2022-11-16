01cc2437a979343d5df5ab568bc4f6b579022890
cyalc <cyalcinsoy@gmail.com>
2022-11-16 19:49:17 +0100

    Add Speller page

---
+++ pages/speller.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+function Speller() {
+    return (
+        <Head>
+            <title>Spelling Alphabet</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+    )
+}
+
+export default Speller
