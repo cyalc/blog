4010a535ab51ae81ca9a591e1cb8a8045585c1a6
cyalc <cyalcinsoy@gmail.com>
2022-11-16 19:55:32 +0100

    Add styled components

--- pages/speller.tsx
+++ pages/speller.tsx
@@ -1,11 +1,20 @@
 import Head from "next/head";
+import styled from "styled-components"
+
+const StyledComponent = styled.div`
+        background: black;
+        color: white;
+    `;
 
 function Speller() {
     return (
-        <Head>
-            <title>Spelling Alphabet</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
+        <>
+            <Head>
+                <title>Spelling Alphabet</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <StyledComponent></StyledComponent>
+        </>
     )
 }
 
